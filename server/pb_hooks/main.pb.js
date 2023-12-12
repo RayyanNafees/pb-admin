fc8b733f547bd9c447a44d18e2a4eeacf6ae9e8c
RayyanNafees <nafees.rayyan@gmail.com>
2023-12-12 19:32:10 +0530

    Added DB + columnar hashing

---
+++ server/pb_hooks/main.pb.js
@@ -0,0 +1,26 @@
+routerAdd("GET", "/hash", (c) => {
+  const dbfilestr = $os.readFile(`${__hooks}/../pb_data/data.db`)
+
+  const hash = $security.md5(dbfilestr)
+  return c.string(200, hash)
+})
+
+routerAdd('GET', '/hash/:col', c => {
+  let col = c.pathParam("col") || 'users'
+
+  const result = arrayOf(new DynamicModel({
+    "id": "",
+  }))
+
+
+  $app.dao().db().select('id').from(col)
+    .all(result)
+  console.log(result)
+  
+  const ids = result.reduce((prev, current) => prev += current.id, '')
+
+  const idhash = $security.md5(ids)
+
+  return c.string(200, idhash)
+})
+
