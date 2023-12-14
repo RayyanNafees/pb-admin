e3552176fc2ae9789eb9f4a93deffb1e72d6e554
RayyanNafees <nafees.rayyan@gmail.com>
2023-12-15 05:11:55 +0530

    Utility for mocking data in DEV mode when Content Collection not found

---
+++ src/utils/mock.ts
@@ -0,0 +1,7 @@
+import { generateMock } from '@anatine/zod-mock';
+// import { UserSchema } from "./schemas";
+import type { defineCollection } from 'astro:content';
+
+const writeModeContent = async(collections: Record<string, ReturnType<typeof defineCollection>>) => {
+
+}
