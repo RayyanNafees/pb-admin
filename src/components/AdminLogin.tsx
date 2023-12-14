9e4e44ef98363b14120a0cb8c1db84fa24df9fd3
RayyanNafees <nafees.rayyan@gmail.com>
2023-12-15 05:11:15 +0530

    Admin Authentication

---
+++ src/components/AdminLogin.tsx
@@ -0,0 +1,86 @@
+import { useEffect, useState } from 'preact/hooks'
+import pb, { adminAuth } from '../lib/pb'
+import type { ClientResponseError } from 'pocketbase'
+
+export default () => {
+  useEffect(() => {
+    if (pb.authStore.isAdmin) window.location.replace('/admin')
+  }, [])
+
+  const [loading, setLoading] = useState(false)
+
+  const [emailError, setEmailError] = useState('')
+  const [passError, setPassError] = useState('')
+
+  const [invalid, setInvalid] = useState<null | 'true' | 'false'>(null)
+  const [invalidPass, setInvalidPass] = useState<null | 'true' | 'false'>(null)
+
+  const handleSubmit = async (e: SubmitEvent) => {
+    e.preventDefault()
+
+    setLoading(true)
+
+    const form = e.currentTarget as HTMLFormElement
+    const { email, pass } = Object.fromEntries(
+      new FormData(form).entries()
+    ) as { email: string; pass: string }
+
+    await adminAuth(email, pass)
+      .then((user) => {
+        invalid ?? setInvalid('false')
+
+        console.log({ user, authStore: pb.authStore })
+        if (pb.authStore.isAdmin) window.location.href = '/admin'
+      })
+      .catch((e: ClientResponseError) => {
+        console.log({ e })
+        if (e.status === 400) {
+          const { email, password } = e.data.data
+          if (email) {
+            setInvalid('true')
+            setEmailError(email.message)
+          }
+          if (password) {
+            setInvalidPass('true')
+            setPassError(password.message)
+          }
+        }
+      })
+
+    setLoading(false)
+  }
+
+  return (
+    <main className='container' style={{ marginTop: 50, width: '30%' }}>
+      <h1 aria-busy={loading}>Admin Login</h1>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          placeholder='Email'
+          aria-placeholder='Email'
+          name='email'
+          type='email'
+          aria-invalid={invalid ?? 'grammar'}
+        />
+        {invalid === 'true' && <small className='error'>{emailError}</small>}
+
+        <input
+          name='pass'
+          type='password'
+          placeholder='Password'
+          aria-placeholder='Password'
+          autoComplete={'true'}
+          aria-invalid={invalidPass ?? 'grammar'}
+        />
+        {invalidPass === 'true' && <small className='error'>{passError}</small>}
+
+        <button
+          disabled={loading}
+          aria-busy={String(loading) as 'true' | 'false'}
+        >
+          Login
+        </button>
+      </form>
+    </main>
+  )
+}
