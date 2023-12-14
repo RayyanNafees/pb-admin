69b2f8ace85d0b255c6f3fe4067202d4b43bfcd8
RayyanNafees <nafees.rayyan@gmail.com>
2023-12-14 13:49:10 +0530

    Added sentry logging for devtoolbar

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,19 @@
 import million from 'million/compiler';
 import { defineConfig } from 'astro/config';
-
 import preact from "@astrojs/preact";
+
+import sentry from "@sentry/astro";
+import spotlightjs from "@spotlightjs/astro";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()],
+  integrations: [preact(), sentry(), spotlightjs()],
   outDir: 'server/pb_public',
   vite: {
-    plugins: [million.vite({ mode: 'preact', server: true, auto: true })]
+    plugins: [million.vite({
+      mode: 'preact',
+      server: true,
+      auto: true
+    })]
   }
-})
+});
